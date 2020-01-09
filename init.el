9877a07d72bc6a6b78108a4d6327cee1d208f495
Boarders <callan.mcgill@gmail.com>
2020-01-09 18:37:31 -0500

    Update config

--- init.el
+++ init.el
@@ -55,7 +55,9 @@
 (global-set-key (kbd "C-c s") 'replace-string)
 (global-unset-key (kbd "C-i"))
 (global-set-key (kbd "<C-return>") 'newline)
+(global-unset-key (kbd "C-x C-u"))
 (require 'package)
+(setq package-check-signature 'nil)
 ;; make whitespace-mode use just basic coloring
 ;;(set-face-attribute 'whitespace-space nil :background nil :foreground "gray30")
 (setq whitespace-display-mappings
@@ -73,47 +75,47 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
-   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#657b83"])
+  ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#657b83"])
  '(compilation-message-face (quote default))
  '(cua-global-mark-cursor-color "#2aa198")
  '(cua-normal-cursor-color "#839496")
  '(cua-overwrite-cursor-color "#b58900")
  '(cua-read-only-cursor-color "#859900")
  '(custom-safe-themes
-   (quote
-    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+  (quote
+   ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(fci-rule-color "#073642")
  '(haskell-process-log t)
  '(haskell-process-suggest-remove-import-lines t)
  '(haskell-process-type (quote cabal-new-repl))
  '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
  '(highlight-symbol-colors
-   (--map
-    (solarized-color-blend it "#002b36" 0.25)
-    (quote
-     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
+  (--map
+   (solarized-color-blend it "#002b36" 0.25)
+   (quote
+    ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
  '(highlight-symbol-foreground-color "#93a1a1")
  '(highlight-tail-colors
-   (quote
-    (("#073642" . 0)
-     ("#546E00" . 20)
-     ("#00736F" . 30)
-     ("#00629D" . 50)
-     ("#7B6000" . 60)
-     ("#8B2C02" . 70)
-     ("#93115C" . 85)
-     ("#073642" . 100))))
+  (quote
+   (("#073642" . 0)
+    ("#546E00" . 20)
+    ("#00736F" . 30)
+    ("#00629D" . 50)
+    ("#7B6000" . 60)
+    ("#8B2C02" . 70)
+    ("#93115C" . 85)
+    ("#073642" . 100))))
  '(hl-bg-colors
-   (quote
-    ("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00")))
+  (quote
+   ("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00")))
  '(hl-fg-colors
-   (quote
-    ("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
+  (quote
+   ("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
  '(inhibit-startup-screen t)
  '(magit-diff-use-overlays nil)
  '(package-selected-packages
-   (quote
-    (god-mode psc-ide racket-mode hungry-delete solarized-theme multiple-cursors merlin tuareg use-package dante rust-mode idris-mode haskell-mode)))
+  (quote
+   (proof-general god-mode psc-ide racket-mode hungry-delete solarized-theme multiple-cursors merlin tuareg use-package dante rust-mode idris-mode haskell-mode)))
  '(pos-tip-background-color "#073642")
  '(pos-tip-foreground-color "#93a1a1")
  '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
@@ -121,29 +123,29 @@
  '(term-default-fg-color "#839496")
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
-   (quote
-    ((20 . "#dc322f")
-     (40 . "#c85d17")
-     (60 . "#be730b")
-     (80 . "#b58900")
-     (100 . "#a58e00")
-     (120 . "#9d9100")
-     (140 . "#959300")
-     (160 . "#8d9600")
-     (180 . "#859900")
-     (200 . "#669b32")
-     (220 . "#579d4c")
-     (240 . "#489e65")
-     (260 . "#399f7e")
-     (280 . "#2aa198")
-     (300 . "#2898af")
-     (320 . "#2793ba")
-     (340 . "#268fc6")
-     (360 . "#268bd2"))))
+  (quote
+   ((20 . "#dc322f")
+    (40 . "#c85d17")
+    (60 . "#be730b")
+    (80 . "#b58900")
+    (100 . "#a58e00")
+    (120 . "#9d9100")
+    (140 . "#959300")
+    (160 . "#8d9600")
+    (180 . "#859900")
+    (200 . "#669b32")
+    (220 . "#579d4c")
+    (240 . "#489e65")
+    (260 . "#399f7e")
+    (280 . "#2aa198")
+    (300 . "#2898af")
+    (320 . "#2793ba")
+    (340 . "#268fc6")
+    (360 . "#268bd2"))))
  '(vc-annotate-very-old-color nil)
  '(weechat-color-list
-   (quote
-    (unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83"))))
+  (quote
+   (unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83"))))
 (setq package-archives
       '(("GNU ELPA"     . "http://elpa.gnu.org/packages/")
         ("MELPA Stable" . "https://stable.melpa.org/packages/")
@@ -174,21 +176,9 @@
          (shell-command-to-string "agda-mode locate")))
 
 (eval-after-load 'haskell-mode '(progn
-;;  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
-  (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
-  (define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
-  (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
-  (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
-  (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)
-  (define-key haskell-mode-map (kbd "C-c C-u") "undefined")))
+  (define-key haskell-mode-map (kbd "C-c C-u") "undefined")
+  (define-key haskell-mode-map (kbd "C-c C-SPC") `comment-region)))
 
-
-
-(eval-after-load 'haskell-cabal '(progn
-  (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
-  (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
-  (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
-  (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)))
 
 (require 'rust-mode)
 (require 'use-package)
@@ -205,8 +195,8 @@
 (add-hook 'agda2-mode-hook
   (lambda ()
     (remove-electric-indent-mode)
-    (define-key agda2-mode-map (kbd "C-i") nil)
-    (define-key agda2-mode-map (kbd "RET") 'newline-and-indent)))
+    (define-key agda2-mode-map (kbd "C-i") nil)))
+;;    (define-key agda2-mode-map (kbd "RET") 'newline-and-indent)))
 
 (add-hook 'python-mode-hook
   (lambda ()
@@ -246,3 +236,6 @@
 (add-to-list 'load-path "~/.emacs.d/lib/purescript-mode/")
 (require 'purescript-mode-autoloads)
 (add-to-list 'Info-default-directory-list "~/.emacs.d/lib/purescript-mode/")
+
+;; coq stuff
+(load "~/.emacs.d/lisp/PG/generic/proof-site")
